7cca1206c276501603c34651d937ffc9be6707f0
fcastaneda9725 <f.castanedat@gmail.com>
2018-04-16 12:56:23 -0500

    Configuración ya realizada con anterioridad, funciona

---
+++ componente1.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Componente1 extends React.Component {
+  constructor(){
+    super()
+  }
+  render(){
+    return(
+      <div>
+        <span> Hola Soy un componente! Todo pedorro de REACT </span>
+      </div>
+    )
+  }
+}
+
+export default Componente1;
--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App.jsx';
+import Componente1 from './componente1.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Componente1 />, document.getElementById('componente'));
