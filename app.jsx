b6dbc9168f9dda96a291841dc407d571345bd800
fcastaneda9725 <f.castanedat@gmail.com>
2018-04-16 11:15:39 -0500

    Mi configuración

---
+++ app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+    render( ) {
+      return (
+        <div>
+            Componente Principal
+          </div>
+      );
+    }
+}
+
+export default App; 
